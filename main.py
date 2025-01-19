1b267b304370f8be170c5712949d2536e3715059
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2025-01-19 01:03:59 +0100

    remove filtering, optimize set encoding, add last workout for context

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import os
 import uuid
 from typing import List
 from dotenv import load_dotenv
+
 load_dotenv()
+
+from db import get_latest_workout
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
-from workout_response import WorkoutResponse, put_workout_in_db
+from workout_response import WorkoutResponse, put_workout_in_db, convert_workout_to_prompt_format
 
 from vlm_request import get_vlm_feedback, encode_image_to_base64, nebius_client, json_llm_request
 
@@ -40,10 +43,13 @@ class WorkoutForm(BaseModel):
 
 @app.post("/make-workout")
 async def make_workout(workout_form: WorkoutForm):
-    def get_exercises(workout_form: WorkoutForm) -> WorkoutResponse:
-        def check_substrings(line, substrings):
-            return any(substr in line for substr in substrings)
+    last_workout = None
+    try:
+        last_workout = convert_workout_to_prompt_format(await get_latest_workout(workout_form.user_id))
+    except Exception as e:
+        print(f"Error fetching last workout: {e}")
 
+    def get_exercises(workout_form: WorkoutForm) -> WorkoutResponse:
         prompt = 'You are a personal trainer creating a workout plan for a client.'
         prompt += f'\nYour client is a {workout_form.age}-year-old and weighs {workout_form.weight} kg.'
         prompt += (f'\nCreate an approximately {workout_form.length}-minute '
@@ -55,12 +61,16 @@ async def make_workout(workout_form: WorkoutForm):
         prompt += f"\n\nClient's comment: {workout_form.comment}"
         prompt += '\nUse the following exercises:\n'
         with open('data/exercise_summary.txt', 'r') as f:
-            lines = f.readlines()
-            filtered_lines = list(filter(lambda line: check_substrings(line, workout_form.muscle_groups), lines))
-            prompt += ''.join(filtered_lines)
-        prompt += '\nOutput the exercises in the provided json format.'
+            prompt += ''.join(f.readlines())
+        prompt += '\nOutput the exercises in the provided json format. Write set as "weight x reps". Vary the reps and weight between sets.'
         prompt += '\nUse 0 weight for bodyweight exercises.'
         prompt += '\nIn the description, include a short (2 sentences max) explanation of why you created the workout like this.'
+        if last_workout:
+            try:
+                prompt += '\nHere is the last workout of this user:\n'
+                prompt += json.dumps(last_workout.dict(), indent=4)
+            except Exception as e:
+                print(f"Error adding last workout to prompt: {e}")
         return json_llm_request(prompt, WorkoutResponse)
 
     workout = get_exercises(workout_form)
